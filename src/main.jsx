0cf52c6bb32af694556ccf9b0796690e5d7dfcf2
luthfianhar <anharluthfi35@gmail.com>
2026-01-30 09:43:03 +0700

    asd

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import AlertButton from './components/Button/AlertButton.jsx'
-import DaftarNama from './components/DaftarNama/DaftarNama.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import AlertButton from "./components/Button/AlertButton.jsx";
+import DaftarNama from "./components/DaftarNama/DaftarNama.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-     <AlertButton text="click" message="Button clicked"/>
-     <DaftarNama />
-    <App />
+    <App>
+      <AlertButton text="click" message="Button clicked" />
+      <DaftarNama />
+    </App>
   </StrictMode>,
-)
+);
